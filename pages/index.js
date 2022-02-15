d91a5a1d41777be3c0f4a4595af28f3219c4d73d
Paul Letreguilly <pletreguilly@gmail.com>
2022-02-15 23:08:36 +0100

    added some stuff

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function Home() {
+  // will postpone this for some days
+
   return (
     <div className="container">
       <Head>
